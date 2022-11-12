6b3a380ec41d372785c0a0a14e694437b403488f
Mariam-Antidze <mariam.antidze21@gmail.com>
2022-11-12 17:05:10 +0400

    Initial Commit

---
+++ Lecture_10_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture_10_Project
+//
+//  Created by Mariam Antidze on 12.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
